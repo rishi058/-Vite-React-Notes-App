42505c1ee30155903db24339e9b3b83d9db7b02b
Rishi <rishi.helloworld@gmail.com>
2024-04-13 12:28:00 +0530

    Fixed oveflow in grid

--- src/pages/homePage/components/Card.jsx
+++ src/pages/homePage/components/Card.jsx
@@ -4,11 +4,15 @@ function Card({ title, content }) {
   return (
     <div className="w-[265px] h-[350px] bg-white/20 backdrop-blur-md border-2 pb-4 border-white/20 rounded-md p-4 m-4">
       <h3
-        className="text-3xl font-bold mb-4 text-slate-800 overflow-hidden text-ellipsis"
+        className="text-2xl font-bold mb-4 text-slate-800 overflow-hidden text-ellipsis whitespace-nowrap"
       >
         {title}
       </h3>
-      <p className="text-lg overflow-hidden text-ellipsis">
+      <p className="text-xl overflow-hidden text-ellipsis text-left" style={{
+        display: '-webkit-box',
+        WebkitLineClamp: 10,
+        WebkitBoxOrient: 'vertical',
+      }}>
         {content}
       </p>
     </div>
